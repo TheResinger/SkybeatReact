e8759dcbdd69aa056a2f69d69aac25e30a77d948
Benjamin Morrow <morrowbenjamin42@gmail.com>
2021-11-07 02:26:20 -0500

    Why was it ignoring the build folder

---
+++ client/build/precache-manifest.84897730aaa5afd2dafc0d9383f5f7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34326e0b3a1284777c791661a42c5836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2ff85ad234e4273b897",
+    "url": "/static/css/main.ce2d0aab.chunk.css"
+  },
+  {
+    "revision": "1ea81b850572fdea3825",
+    "url": "/static/js/2.97257399.chunk.js"
+  },
+  {
+    "revision": "3bb8f4be79104c3617df3eac6c3c9584",
+    "url": "/static/js/2.97257399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ff85ad234e4273b897",
+    "url": "/static/js/main.311bc6ac.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
